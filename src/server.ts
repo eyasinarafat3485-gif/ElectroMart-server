f1764b4fce871541581b83416eeb3d962f184f39
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-07-10 17:01:10 +0600

    item added post api create

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,17 @@
+import dns from "node:dns";
+dns.setServers(["8.8.8.8", "8.8.4.4"]);
+
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv";
-
+import { MongoClient, ServerApiVersion } from "mongodb";
 dotenv.config();
+
+const uri = process.env.MONGODB_URI;
+
+if (!uri) {
+  throw new Error("MONGODB_URI is missing in the .env file");
+}
 
 const app = express();
 const port = process.env.PORT || 5000;
@@ -10,10 +19,40 @@ const port = process.env.PORT || 5000;
 app.use(cors());
 app.use(express.json());
 
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  },
+});
+
+const run = async () => {
+  try {
+    await client.connect();
+    const db = client.db("ElectroMart");
+    const itemCollection = db.collection("items")
+
+    // items added
+    app.post('/api/items', async (req, res)=>{
+        const items = req.body;
+        const result = await itemCollection.insertOne(items);
+        res.send(result);
+    })
+
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // await client.close();
+  }
+};
+
+run().catch(console.dir);
+
 app.get("/", (_req, res) => {
   res.send("ElectroMart Server is running on 5000...");
 });
 
 app.listen(port, () => {
-  console.log(`🚀 Server running on http://localhost:${port}`);
+  console.log(`Server running on http://localhost:${port}`);
 });
