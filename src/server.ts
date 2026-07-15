521dd2e9848b9f708763d1d1c402308ef4fdc6b8
eyasinarafat3485-gif <eyasinarafat3485@gmail.com>
2026-07-15 09:16:26 +0600

    fix: comment out DNS override causing JWKS fetch timeout

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,7 @@
 import dns from "dns";
-if (dns && typeof dns.setServers === "function") {
-  dns.setServers(["8.8.8.8", "8.8.4.4"]);
-}
+// if (dns && typeof dns.setServers === "function") {
+//   dns.setServers(["8.8.8.8", "8.8.4.4"]);
+// }
 
 import express, { Request, Response, NextFunction } from "express";
 import cors from "cors";
